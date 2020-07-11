fc230ff40fd5df0c5d23275846953032f80c63f8
Lucifer <chocolateIronman98@gmail.com>
2020-07-11 15:20:26 +0100

    attempt for csv 5

--- service/SplitsService.js
+++ service/SplitsService.js
@@ -14,22 +14,6 @@ exports.getSplits = function(args,res,next) {
     ).then(
       (result)=>{
         httpUtil.endHttpOK(result,res);
-        const options = { 
-          fieldSeparator: ',',
-          quoteStrings: '"',
-          decimalSeparator: '.',
-          showLabels: true, 
-          showTitle: true,
-          title: 'My Awesome CSV',
-          useTextFile: false,
-          useBom: true,
-          useKeysAsHeaders: true,
-          // headers: ['Column 1', 'Column 2', etc...] <-- Won't work with useKeysAsHeaders present!
-        };
-       
-      const csvExporter = new ExportToCsv(options);
-       
-      csvExporter.generateCsv(result);
       }
     ).catch(
       (error)=>{
